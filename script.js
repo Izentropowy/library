c01f119e4eee422263eb9539d04210e8c7c35149
Izentropowy <119621854+Izentropowy@users.noreply.github.com>
2024-02-11 12:41:37 +0100

    Fix library references

--- script.js
+++ script.js
@@ -1,131 +1,112 @@
-const addBookButton = document.querySelector('.add-book');
-const insideModal = document.querySelector('.adding-box');
-const outsideModal = document.querySelector('.modal');
-const main = document.querySelector('.main');
-const form = document.querySelector('form');
-const submitButton = document.getElementById('submit');
+const addBookButton = document.querySelector(".add-book");
+const insideModal = document.querySelector(".adding-box");
+const outsideModal = document.querySelector(".modal");
+const main = document.querySelector(".main");
+const form = document.querySelector("form");
+const submitButton = document.getElementById("submit");
 
 class Book {
-    constructor(title, author, pages, isRead) {
-        this.title = title,
-        this.author  = author,
-        this.pages = pages,
-        this.isRead = isRead
-    }
-    
-    toggleStatus() {
-        this.isRead = !this.isRead;
-    }
+  constructor(title, author, pages, isRead) {
+    (this.title = title),
+      (this.author = author),
+      (this.pages = pages),
+      (this.isRead = isRead);
+  }
+
+  toggleStatus() {
+    this.isRead = !this.isRead;
+  }
 }
 
 class Library {
-    constructor() {
-        this.books = []
-    }
+  constructor() {
+    this.books = [];
+  }
 }
 
-/* 
-Library:
-    - addBook
-    - setIndexes
-    - markStatus
-
-DisplayController:
-    - openModal
-    - closeModal
-    - insideModalClicked
-    - outsideModalClicked
-    - createCard
-    - removeCard
-    - displayBooks
- */
-
-let myLibrary = new Library()
+let myLibrary = new Library();
 
 function openModal() {
-    form.classList.add('form-visible');
+  form.classList.add("form-visible");
 }
 
 function closeModal() {
-    form.classList.remove('form-visible');
-    form.reset();
+  form.classList.remove("form-visible");
+  form.reset();
 }
 
 function insideModalClicked(e) {
-    e.stopPropagation();
-    e.stopImmediatePropagation();
+  e.stopPropagation();
+  e.stopImmediatePropagation();
 }
 
 function outsideModalClicked() {
-    closeModal();
+  closeModal();
 }
 
 function addBookToLibrary(e) {
-    form.reportValidity();
-    if (form.reportValidity()){
-        e.preventDefault();
-        const title = document.getElementById('title').value;
-        const author = document.getElementById('author').value;
-        const pages = document.getElementById('pages').value;
-        const isRead = document.getElementById('check').checked;
-        const newBook = new Book(title, author, pages, isRead);
-        myLibrary.push(newBook);
-    }
+  form.reportValidity();
+  if (form.reportValidity()) {
+    e.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const isRead = document.getElementById("check").checked;
+    const newBook = new Book(title, author, pages, isRead);
+    myLibrary.books.push(newBook);
+  }
 }
 
 function createCard(book) {
-    var cardDiv = document.createElement("div");
-    let statusClass = book.isRead ? "read" : "not-read";
-    cardDiv.classList.add("card");
-    const cardContent =  `<h2>${book.title}</h2>
+  var cardDiv = document.createElement("div");
+  let statusClass = book.isRead ? "read" : "not-read";
+  cardDiv.classList.add("card");
+  const cardContent = `<h2>${book.title}</h2>
                         <h3>${book.author}</h3>
                         <h4>${"Pages: " + book.pages} </h4>
                         <div id="${book.index}">
                             <button class="status ${statusClass}"></button>
                             <button class="remove">Remove</button>
-                        </div>`
-    cardDiv.innerHTML = cardContent;
-    main.appendChild(cardDiv);
+                        </div>`;
+  cardDiv.innerHTML = cardContent;
+  main.appendChild(cardDiv);
 }
 
 function displayBooks() {
-    main.innerHTML = "";
-    myLibrary.forEach(element => createCard(element));
+  main.innerHTML = "";
+  myLibrary.books.forEach((element) => createCard(element));
 }
 
 function setIndexes() {
-    myLibrary.forEach(element => {
-        element.index = myLibrary.indexOf(element);
-    });
+  myLibrary.books.forEach((element) => {
+    element.index = myLibrary.books.indexOf(element);
+  });
 }
 
 function removeCard(e) {
-    if (e.target.classList.contains('remove')) {
-        myLibrary.splice(e.target.parentNode.id, 1);
-        setIndexes();
-        displayBooks();
-    }
+  if (e.target.classList.contains("remove")) {
+    myLibrary.books.splice(e.target.parentNode.id, 1);
+    setIndexes();
+    displayBooks();
+  }
 }
 
 function markStatus(e) {
-    if (e.target.classList.contains('status')) {
-        let currentBook = myLibrary[e.target.parentNode.id]
-        currentBook.toggleStatus();
-        displayBooks();
-    }
+  if (e.target.classList.contains("status")) {
+    let currentBook = myLibrary.books[e.target.parentNode.id];
+    currentBook.toggleStatus();
+    displayBooks();
+  }
 }
 
-addBookButton.addEventListener('click', openModal);
-insideModal.addEventListener('click', insideModalClicked);
-outsideModal.addEventListener('click', outsideModalClicked);
-submitButton.addEventListener('click', addBookToLibrary);
-submitButton.addEventListener('click', setIndexes);
-submitButton.addEventListener('click', displayBooks);
-submitButton.addEventListener('click', () => {
-    if (form.reportValidity()) closeModal();
+addBookButton.addEventListener("click", openModal);
+insideModal.addEventListener("click", insideModalClicked);
+outsideModal.addEventListener("click", outsideModalClicked);
+submitButton.addEventListener("click", addBookToLibrary);
+submitButton.addEventListener("click", setIndexes);
+submitButton.addEventListener("click", displayBooks);
+submitButton.addEventListener("click", () => {
+  if (form.reportValidity()) closeModal();
 });
-document.body.addEventListener('click', removeCard);
-document.body.addEventListener('click', markStatus);
-
-
-
+document.body.addEventListener("click", removeCard);
+document.body.addEventListener("click", markStatus);
